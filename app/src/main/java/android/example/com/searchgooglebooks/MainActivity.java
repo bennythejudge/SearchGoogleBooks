57741161c53a7a6ef908e6faa462610406fe3584
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-04-04 23:10:55 +0200

    Work on main activity

--- app/src/main/java/android/example/com/searchgooglebooks/MainActivity.java
+++ app/src/main/java/android/example/com/searchgooglebooks/MainActivity.java
@@ -2,12 +2,29 @@ package android.example.com.searchgooglebooks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button searchBtn;
+    private EditText search_term;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_term = findViewById(R.id.search_term);
+        searchBtn = findViewById(R.id.search_button);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search_term = findViewById(R.id.search_term);
+                Log.d("onclicklistener", "someone clicked me: " + search_term.getText());
+            }
+        });
     }
 }
